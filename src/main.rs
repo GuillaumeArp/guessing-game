1fde3adc287264b1c3900684a38578e8445a5370
Guillaume Arp <guillaume.arp@gmail.com>
2022-10-23 22:04:35 +0200

    updated build

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number between 1 and 100!:\n");
+
+    println!("Please input your guess here.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}");
+
 }
